78628a88f7cbedd9b891ed9c64bdf1bf16ef459e
Chalin-07 <chalincuevas115@gmail.com>
2025-04-27 21:16:07 -0500

    Update registrar_efi.php

--- registrar_efi.php
+++ registrar_efi.php
@@ -3,113 +3,132 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styles_efi.css">
+    <link rel="stylesheet" href="styles/styles_efi.css"> <!-- Enlace a la hoja de estilos para la página -->
     <title>Document</title>
 </head>
 <body>
     
     <?php   
+        // Se incluye el encabezado común a todas las páginas (header_2.html).
         require_once 'content_princi/header_2.html';   
     ?>
 
-    
+    <!-- Formulario para registrar la eficiencia -->
     <form action="registrar_efi_2.php"  method="post" enctype="multipart/form-data">
         <div class="reg">
 
             <div id="formu">
                 
-            <a href="operario.php"><img id="x" src="img/equis.jpg" alt=""></a><br><br>
+                <!-- Botón para regresar a la página anterior (operario.php) -->
+                <a href="operario.php"><img id="x" src="img/equis.jpg" alt=""></a><br><br>
 
-                <label for="" >
-                    Cantidad realizada<input type="number" name="cantidad" id="can_rea" required>
+                <!-- Campo para ingresar la cantidad realizada -->
+                <label for="">
+                    Cantidad realizada
+                    <input type="number" name="cantidad" id="can_rea" required>
                 </label>
                 <br><br><br>
-                <label for="" >
-                   Tiempo gastado en min<input type="number" name="tiempo" id="tie_gas" required>
+
+                <!-- Campo para ingresar el tiempo gastado -->
+                <label for="">
+                   Tiempo gastado en min
+                   <input type="number" name="tiempo" id="tie_gas" required>
                 </label>
                 <br><br><br>
-                <label for="" >
-                   Fecha<input type="date" name="fecha" id="fec_ind" required>
+
+                <!-- Campo para ingresar la fecha -->
+                <label for="">
+                   Fecha
+                   <input type="date" name="fecha" id="fec_ind" required>
                 </label>
                 <br><br><br>
                 
+                <!-- Dropdown para seleccionar el detalle de la orden -->
                 <label for="">
+                    <?php
+                        // Conexión a la base de datos
+                        require_once 'conexion.php';
 
-                <?php
-                     require_once 'conexion.php';
+                        // Se hace una consulta para obtener los detalles de las órdenes asignadas al usuario
+                        $respuesta = mysqli_query($conex, "SELECT d.id_det, u.nom_usu, o.nro_ord, o.fec_ent, o.nom_pro, o.can_tot, o.pro_ord
+                                                            FROM detalle_orden d
+                                                            JOIN usuario u ON d.id_usu_fk = u.id_usu
+                                                            JOIN orden o ON d.id_ord_fk = o.id_ord
+                                                            WHERE u.id_usu = '$_REQUEST[cod]'") 
+                                                            or die("error".mysqli_error($conex));
 
-                     $respuesta = mysqli_query($conex," SELECT d.id_det , u.nom_usu, o.nro_ord,o.fec_ent,o.nom_pro, o.can_tot, o.pro_ord
-                                                        FROM detalle_orden d
-                                                        JOIN usuario u ON d.id_usu_fk = u.id_usu
-                                                        JOIN orden o ON d.id_ord_fk = o.id_ord
-                                                        WHERE u.id_usu = '$_REQUEST[cod]'")or die("error".mysqli_error($conex));
-             
-             
-             
-             $datos = [];
+                        $datos = [];
 
-             echo "Detalle orden<select name='orden' id='orden' onchange='llenarInputs()'>";
-             echo "<option value=''>seleccione</option>";
-             
-             while ($reg = mysqli_fetch_array($respuesta)) {
-                 $datos[$reg['id_det']] = $reg;
-                 echo "<option value='" . $reg['id_det'] . "'>" . $reg['id_det'] . "</option>";
-             }
-             echo "</select><br><br>";
-            
-                ?>
+                        // Desplegar el dropdown con los detalles de las órdenes
+                        echo "Detalle orden<select name='orden' id='orden' onchange='llenarInputs()'>";
+                        echo "<option value=''>seleccione</option>";
 
-                <div class="orden"> 
-                Numero orden: <input type="text" id="nro_ord" disabled><br>
-                Fecha entrega: <input type="text" id="fec_ent" disabled><br>
-                Producto: <input type="text" id="nom_pro" disabled><br>
-                Cantidad: <input type="text" id="can_tot" disabled><br>
-                Proceso: <input type="text" id="pro_ord" disabled><br>
-                </div>
+                        // Recorrer los resultados y mostrar cada detalle de orden en el dropdown
+                        while ($reg = mysqli_fetch_array($respuesta)) {
+                            $datos[$reg['id_det']] = $reg;
+                            echo "<option value='" . $reg['id_det'] . "'>" . $reg['id_det'] . "</option>";
+                        }
+                        echo "</select><br><br>";
+                    ?>
 
-                <script>
-                    // esta constante datos permite guardar mis datos y enviarlos como un objeto a javascript
-                    const datos = <?php echo json_encode($datos); ?>;
-                </script>
+                    <!-- Div que muestra los detalles de la orden seleccionada -->
+                    <div class="orden"> 
+                        Numero orden: <input type="text" id="nro_ord" disabled><br>
+                        Fecha entrega: <input type="text" id="fec_ent" disabled><br>
+                        Producto: <input type="text" id="nom_pro" disabled><br>
+                        Cantidad: <input type="text" id="can_tot" disabled><br>
+                        Proceso: <input type="text" id="pro_ord" disabled><br>
+                    </div>
+
+                    <!-- Se pasa la variable $datos como un objeto JavaScript -->
+                    <script>
+                        const datos = <?php echo json_encode($datos); ?>;
+                    </script>
 
                 </label>
+
+                <!-- Dropdown para seleccionar el producto estandar -->
                 <label for="">
-            <?php
-                   
+                    <?php
+                        // Se consulta la tabla promedio para obtener los productos estándar
+                        $registro = mysqli_query($conex, "SELECT * FROM promedio") 
+                                    or die("error".mysqli_error($conex));
 
-                $registro = mysqli_query($conex,"select*from promedio")or die("error".mysqli_error($conex));
-             
-             
-             
-             $dat = [];
+                        $dat = [];
 
-             echo "<br>Estandar<select name='estandar' id='estandar' onchange='llenarInputsp()'>";
-             echo "<option value=''>seleccione</option>";
-             
-             while ($reg = mysqli_fetch_array($registro)) {
-                 $dat[$reg['id_pro']] = $reg;
-                 echo "<option value='" . $reg['id_pro'] . "'>" . $reg['id_pro'] . "</option>";
-             }
-             echo "</select><br><br>";
-            
-                ?>
+                        // Desplegar el dropdown con los productos estándar
+                        echo "<br>Estandar<select name='estandar' id='estandar' onchange='llenarInputsp()'>";
+                        echo "<option value=''>seleccione</option>";
 
-                <div class="orden"> 
-                Producto estandar: <input type="text" id="pro_pro" disabled><br>
-                Proceso estandar: <input type="text" id="act_pro" disabled><br>
-                </div>
+                        // Recorrer los resultados y mostrar cada estándar en el dropdown
+                        while ($reg = mysqli_fetch_array($registro)) {
+                            $dat[$reg['id_pro']] = $reg;
+                            echo "<option value='" . $reg['id_pro'] . "'>" . $reg['id_pro'] . "</option>";
+                        }
+                        echo "</select><br><br>";
+                    ?>
 
-                <script>
-                    // esta constante datos permite guardar mis datos y enviarlos como un objeto a javascript
-                    const dat = <?php echo json_encode($dat); ?>;
-                </script>
+                    <!-- Div que muestra los detalles del producto estándar -->
+                    <div class="orden"> 
+                        Producto estandar: <input type="text" id="pro_pro" disabled><br>
+                        Proceso estandar: <input type="text" id="act_pro" disabled><br>
+                    </div>
 
+                    <!-- Se pasa la variable $dat como un objeto JavaScript -->
+                    <script>
+                        const dat = <?php echo json_encode($dat); ?>;
+                    </script>
                 </label>
                 <br><br>
-                <label for="" >
-                    <input type="submit" value="cargar" id="carg"><input type="reset" value="limpiar" id="limp">
+
+                <!-- Botones para enviar el formulario o limpiar los campos -->
+                <label for="">
+                    <input type="submit" value="cargar" id="carg">
+                    <input type="reset" value="limpiar" id="limp">
                 </label>
                 <br><br>
+
+                <!-- Botón para volver a la página de supervisor -->
                 <label for="">
                     <button id="button"><a href="supervisor.php">Volver</a></button>
                 </label>
@@ -117,8 +136,13 @@
         </div>
     </form>
 
-        <script src="javascript/funciones.js"></script>
+    <!-- Archivo JavaScript que contiene las funciones necesarias para la página -->
+    <script src="javascript/funciones.js"></script>
 
     <?php   
+        // Se incluye el pie de página común a todas las páginas (footer.html).
         require_once 'content_princi/footer.html';  
     ?>
+
+</body>
+</html>
