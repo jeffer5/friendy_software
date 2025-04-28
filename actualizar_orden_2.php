22db0d842ac5c9fdcaa1d06733c5d6f2878c3994
Chalin-07 <chalincuevas115@gmail.com>
2025-04-27 20:49:52 -0500

    Update actualizar_orden_2.php

--- actualizar_orden_2.php
+++ actualizar_orden_2.php
@@ -9,33 +9,40 @@
 <body>
     
     <?php
-
+    // Se incluye el encabezado de la página
     require_once 'content_princi/header_2.html';  
 
-    //conexion a la base de datos
+    // Conexión a la base de datos
     require_once 'conexion.php';
 
-
-    // actualizar registros tabla aprendiz
+    // Actualización de los registros en la tabla 'orden'
+    // Se actualizan varios campos en la tabla 'orden' usando valores pasados por $_REQUEST
     $registro = mysqli_query($conex,"update orden set nro_ord = '$_REQUEST[numero_orden]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
     $registro = mysqli_query($conex,"update orden set nom_cli = '$_REQUEST[nombre_cliente]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
     $registro = mysqli_query($conex,"update orden set fec_ent = '$_REQUEST[fecha_entrega]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
     $registro = mysqli_query($conex,"update orden set nom_pro = '$_REQUEST[nombre_producto]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
     $registro = mysqli_query($conex,"update orden set can_tot = '$_REQUEST[cantidad_tot]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
+    
+    // Línea redundante, actualiza el campo 'can_tot' de nuevo con el mismo valor
     $registro = mysqli_query($conex,"update orden set can_tot = '$_REQUEST[cantidad_tot]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
+    
+    // Actualiza el campo 'pro_ord' (proceso a realizar) en la tabla 'orden'
     $registro = mysqli_query($conex,"update orden set pro_ord = '$_REQUEST[proceso_ord]' where id_ord='$_REQUEST[id_ord]' ") or die ("error".mysqli_error($conex));
 
+    // Mensaje que confirma que los datos han sido actualizados correctamente
     echo '<center><p id=datos>datos actualizados</p><br>';
+?>
 
+<!-- Botón para actualizar otro registro -->
+<button id="volver1"><a href="actualizar_orden.php">Actualizar otro registro</a></button><br><br>
 
-    ?>
+<!-- Botón para volver a la página de supervisor -->
+<button id="volver"><a href="supervisor.php">volver</a></button>
 
-    <button id="volver1"><a href="actualizar_orden.php">Actualizar otro registro</a></button><br><br>
-    <button id="volver"><a href="supervisor.php">volver</a></button>
-
-    <?php
-        require_once 'content_princi/footer.html';
-   ?>
+<?php
+    // Se incluye el pie de página
+    require_once 'content_princi/footer.html';
+?>
 
 </body>
 </html>
